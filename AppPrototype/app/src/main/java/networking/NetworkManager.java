2599aece13e5113f94a8f7c3f38063d4c32d09b2
mitch10e <mitch10e@gmail.com>
2016-03-12 12:08:32 -0700

    updated endpoints

--- AppPrototype/app/src/main/java/networking/NetworkManager.java
+++ AppPrototype/app/src/main/java/networking/NetworkManager.java
@@ -6,9 +6,9 @@ package networking;
 public class NetworkManager {
 
     private static NetworkManager instance = new NetworkManager();
-    private static final String usersEndpoint = "https://tbd456.firebaseio.com/";
-    private static final String listsEndpoint = "https://tbd456lists.firebaseio.com/";
-    private static final String itemsEndpoint = "https://tbd456items.firebaseio.com/";
+    private static final String usersEndpoint = "https://tbd456.firebaseio.com/users";
+    private static final String listsEndpoint = "https://tbd456.firebaseio.com/lists";
+    private static final String itemsEndpoint = "https://tbd456.firebaseio.com/items";
 
     private NetworkManager() {}
 
