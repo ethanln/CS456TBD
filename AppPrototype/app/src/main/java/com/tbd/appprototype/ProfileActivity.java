5e7064350ea80b901bc7b37d4f1320aec3234e2d
Ethan <elguerito90@gmail.com>
2016-03-30 14:16:18 -0600

    styles added

--- AppPrototype/app/src/main/java/com/tbd/appprototype/ProfileActivity.java
+++ AppPrototype/app/src/main/java/com/tbd/appprototype/ProfileActivity.java
@@ -5,6 +5,10 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -19,9 +23,11 @@ import networking.callback.GenericCallback;
 import util.BlobImageLoaderUtil;
 import util.ConvertToBlobUtil;
 import util.CustomImageUtil;
+import util.LoadingScreenUtil;
 import util.UIMessageUtil;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
     final int PIC_CROP = 2;
@@ -33,7 +39,7 @@ public class ProfileActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        /*getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
@@ -42,7 +48,27 @@ public class ProfileActivity extends AppCompatActivity {
             public void onClick(View v) {
                 onBackPressed();
             }
-        });
+        });*/
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        TBDApplication app = (TBDApplication) getApplication();
+
+        // load profile image on the main navigation
+        ImageView profileImage = (ImageView)navigationView.getHeaderView(0).findViewById(R.id.profile_image_drawer);
+        BlobImageLoaderUtil imageLoader = new BlobImageLoaderUtil();
+        imageLoader.loadImage(app.getCurrentUser().getImageURL(), profileImage, 550);
+
+        // set profile name
+        TextView profileName = (TextView)navigationView.getHeaderView(0).findViewById(R.id.profile_name_drawer);
+        profileName.setText(app.getCurrentUser().getUsername());
 
         this.setProfileInfo();
     }
@@ -122,44 +148,77 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my_lists, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        if(id == R.id.action_profile){
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.action_profile) {
             finish();
             startActivity(getIntent());
         }
-        else if(id == R.id.action_friends){
+        else if (id == R.id.action_friends) {
             Intent intent = new Intent(ProfileActivity.this, FriendsActivity.class);
             startActivity(intent);
         }
-        else if(id == R.id.action_my_lists){
+        else if (id == R.id.action_my_lists) {
             Intent intent = new Intent(ProfileActivity.this, MyListsActivity.class);
             startActivity(intent);
         }
-        else if(id == R.id.action_item_requests){
+        else if (id == R.id.action_item_requests) {
             Intent intent = new Intent(ProfileActivity.this, ItemRequestsActivity.class);
             startActivity(intent);
         }
-        else if(id == R.id.action_friend_requests){
+        else if (id == R.id.action_friend_requests) {
             Intent intent = new Intent(ProfileActivity.this, FriendRequestsActivity.class);
             startActivity(intent);
         }
-        else if(id == R.id.action_logout){
+        else if (id == R.id.action_logout) {
+            LoadingScreenUtil.start(ProfileActivity.this, "Logging out...");
             NetworkManager.getInstance().makeLogoutUserRequest(new GenericCallback() {
                 @Override
                 public void callback() {
-                    UIMessageUtil.showResultMessage(getApplicationContext(), "Logging out...");
+                    LoadingScreenUtil.setEndMessage(getApplicationContext(), "Logged out");
                     startActivity(new Intent(ProfileActivity.this, LoginActivity.class));
+                    LoadingScreenUtil.end();
                 }
             });
         }
-        return false;
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
